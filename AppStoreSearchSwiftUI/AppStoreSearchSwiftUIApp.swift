ed65ad9c8a04fb637edfb7e54d1ed20b5c7903c9
proft <proftua@gmail.com>
2024-04-03 23:20:26 +0300

    Initial Commit

---
+++ AppStoreSearchSwiftUI/AppStoreSearchSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreSearchSwiftUIApp.swift
+//  AppStoreSearchSwiftUI
+//
+//  Created by Ivan Morgun on 03.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreSearchSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
